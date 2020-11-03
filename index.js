acab133842c8f3164db609e6a4318b01cfad594a
sunxmaz <sunxmaz@163.com>
2020-11-03 15:54:51 +0800

    init1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http=require('http')
 
 let server=http.createServer((req,res)=>{
     if(req.method==='post'&&req.url==='/webhook'){
-        res.setHeader('Content-Type':'application/json')
+        res.setHeader('Content-Type','application/json')
         res.end(JSON.stringify({ok:true}))
     }else{
         res.end('Not found')
