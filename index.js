81162e108a32a642cc3f09059708498c1b6855d8
sunxmaz <sunxmaz@163.com>
2020-11-03 16:57:52 +0800

    msg

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http=require('http')
 
 let server=http.createServer((req,res)=>{
+    console.log(req.method,req.url)
     if(req.method==='post'&&req.url==='/webhook'){
         res.setHeader('Content-Type','application/json')
         res.end(JSON.stringify({ok:true}))
