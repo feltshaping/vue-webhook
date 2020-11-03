68f06cf21d9cfd6227bc2b993aa7e041d7083455
sunxmaz <sunxmaz@163.com>
2020-11-03 17:21:54 +0800

    msg

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-const http=require('http')
-
+let http=require('http')
+let crypto=require('crypto')
+let SECRET='123456'
+function sign(body){
+    return `sha1=`+crypto.createHmac('sha1',SECRET).update(body)
+}
 let server=http.createServer((req,res)=>{
-    console.log(req.method,req.url)
     if(req.method==='post'&&req.url==='/webhook'){
+        let buffers=[];
+        req.on('data',buffer=>{buffers.push(buffer)})
+        req.on('end',buffer=>{
+            let body=Buffer.concat(buffers);
+            let event=req.header['x-gitHub-event']
+            let signature=req.headers['x-gitHub-signature'];
+            if(signature!==sign(body)){
+                return res.end('Not allowed')
+            }
+        })
         res.setHeader('Content-Type','application/json')
         res.end(JSON.stringify({ok:true}))
     }else{
