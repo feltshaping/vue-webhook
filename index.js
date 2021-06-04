f4181e8aab8669fb3d6f31cadb07bf982c3ff2e4
sunxm <sunxmaz@163.com>
2021-06-04 10:04:38 +0800

    add dep

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
-let http=require('http')
-let crypto=require('crypto')
-let SECRET='123456'
-function sign(body){
-    return `sha1=`+crypto.createHmac('sha1',SECRET).update(body).digest("hex")
+let http = require('http');
+let crypto = require('crypto');
+let SECRET = '123456';
+function sign(body) {
+  return `sha1=` + crypto.createHmac('sha1', SECRET).update(body).digest('hex');
 }
-let server=http.createServer((req,res)=>{
-    if(req.method==='post'&&req.url==='/webhook'){
-        let buffers=[];
-        req.on('data',buffer=>{buffers.push(buffer)})
-        req.on('end',buffer=>{
-            let body=Buffer.concat(buffers);
-            let event=req.header['x-github-event']
-            let signature=req.headers['x-github-signature'];
-            if(signature!==sign(body)){
-                return res.end('Not allowed')
-            }
-        })
-        res.setHeader('Content-Type','application/json')
-        res.end(JSON.stringify({ok:true}))
-    }else{
-        res.end('Not found')
-    }
-})
-server.listen(4000,()=>{
-    console.log('webhook 4000')
-})
+let server = http.createServer((req, res) => {
+  if (req.method === 'post' && req.url === '/webhook') {
+    let buffers = [];
+    req.on('data', (buffer) => {
+      buffers.push(buffer);
+    });
+    req.on('end', (buffer) => {
+      let body = Buffer.concat(buffers);
+      let event = req.header['x-github-event'];
+      let signature = req.headers['x-github-signature'];
+      if (signature !== sign(body)) {
+        return res.end('Not allowed');
+      }
+    });
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({ ok: true }));
+  } else {
+    res.end('Not found');
+  }
+});
+server.listen(4000, () => {
+  console.log('webhook 4000');
+});
