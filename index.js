4316b42250748942566d8dab4381b88c463a32a3
sunxm <sunxmaz@163.com>
2023-09-01 16:22:30 +0800

    parse

--- index.js
+++ index.js
@@ -13,33 +13,29 @@ let server = http.createServer((req, res) => {
     });
     req.on('end', (buffer) => {
       let body = Buffer.concat(buffers);
-      let event = req.headers['x-github-event'];
+      let event = req.header['x-github-event'];
       let signature = req.headers['x-hub-signature'];
-             let payload = JSON.parse(body);
-        console.log('!payload',payload)
-        console.log(payload.repository.name,'push请求')
-
       // console.log(event, signature, sign(body));
-      // if (signature !== sign(body)) {
-      //   return res.end('Not allowed');
-      // }
-      // res.setHeader('Content-Type', 'application/json');
-      // res.end(JSON.stringify({ ok: true }));
-      // if (event == 'push') {
-      //   let payload = JSON.parse(body);
-      //   console.log('!payload',payload)
-      //   console.log(payload.repository.name+'push请求')
+      if (signature !== sign(body)) {
+        return res.end('Not allowed');
+      }
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify({ ok: true }));
+      if (event == 'push') {
+        let payload = JSON.parse(body);
+        console.log('!payload',payload)
+        // console.log(payload.repository.name+'push请求')
 
-      //   let child = spawn('sh', [`./${payload.repository.name}.sh`]);
-      //   let buffers = [];
-      //   child.stdout.on('data', function (buffer) {
-      //     buffers.push(buffer);
-      //   });
-      //   child.stdout.on('end', function (buffer) {
-      //     let log = Buffer.concat(buffers).toString();
-      //     console.log('sh done!',log);
-      //   });
-      // }
+        // let child = spawn('sh', [`./${payload.repository.name}.sh`]);
+        // let buffers = [];
+        // child.stdout.on('data', function (buffer) {
+        //   buffers.push(buffer);
+        // });
+        // child.stdout.on('end', function (buffer) {
+        //   let log = Buffer.concat(buffers).toString();
+        //   console.log('sh done!',log);
+        // });
+      }
     });
   } else {
     res.end('Not found!');
