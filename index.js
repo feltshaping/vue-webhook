d812e0aae6b1874ddf6c9f5b388407dc722d12d5
sunxm <sunxmaz@163.com>
2023-09-01 09:54:17 +0800

    3 years

---
+++ index.js
@@ -0,0 +1,44 @@
+let http = require('http');
+let crypto = require('crypto');
+let { spawn } = require('child_process');
+let SECRET = 'bp123456';
+function sign(body) {
+  return `sha1=` + crypto.createHmac('sha1', SECRET).update(body).digest('hex');
+}
+let server = http.createServer((req, res) => {
+  if (req.method === 'POST' && req.url === '/webhook') {
+    let buffers = [];
+    req.on('data', (buffer) => {
+      buffers.push(buffer);
+    });
+    req.on('end', (buffer) => {
+      let body = Buffer.concat(buffers);
+      let event = req.headers['x-github-event'];
+      let signature = req.headers['x-hub-signature'];
+      console.log(event, signature, sign(body));
+      if (signature !== sign(body)) {
+        return res.end('Not allowed');
+      }
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify({ ok: true }));
+      if (event == 'push') {
+        console.log('push请求')
+        // let payload = JSON.parse(body);
+        // let child = spawn('sh', [`./${payload.repository.name}.sh`]);
+        // let buffers = [];
+        // child.stdout.on('data', function (buffer) {
+        //   buffers.push(buffer);
+        // });
+        // child.stdout.on('end', function (buffer) {
+        //   let log = Buffer.concat(buffers).toString();
+        //   console.log(log);
+        // });
+      }
+    });
+  } else {
+    res.end('Not found');
+  }
+});
+server.listen(4000, () => {
+  console.log('webhook on 4000');
+});
